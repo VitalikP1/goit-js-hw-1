089397939b84813762403e593f42177cda952639
Vitalik <v.podorozhnyuk@hotmail.com>
2020-04-23 23:06:14 +0300

    init

---
+++ Task-1.js
@@ -0,0 +1,33 @@
+<!-- M1-HW1-Task1 -->
+// const name = "Генератор защитного поля"; let price = 1000; alert("'Выбран ",
+// name, ", цена за штуку " price, " кредитов'"); price = 2000; alert("'Выбран ",
+// name, ", цена за штуку " price, " кредитов'");
+
+<!-- M1-HW1-Task2 -->
+// let total; let ordered; if (total < ordered) { alert("На складе не достаточно
+// товаров"); } else { alert("Заказ оформлен, с вами свяжеться наш менеджер"); }
+// const total = 100; const ordered = 50;
+
+<!-- M1-HW1-Task3 -->
+// const ADMIN_PASSWORD = "QWERTY"; let = message = prompt("Введите пароль вашего
+// профиля"); if (ADMIN_PASSWORD = message) { alert("Добро пожаловать!"); } else if
+// (ADMIN_PASSWORD = NaN) { alert("Отменено пользователем!"); } else { alert("Достп
+// запрещён, не верный пароль!"); }
+
+<!-- M1-HW1-Task4 -->
+// let credits = 23580; 
+// const pricePerDroid = 3000; 
+// const droidsWeHave = 25; let
+// droidsTheyWant = prompt("Сколько дроидов вы хотите купить?"); 
+// if (droidsTheyWant = NaN) { 
+//     alert("Отменено пользователем!"); 
+// } else if (droidsTheyWant > 0 <= droidsWeHave) { 
+//     let totalPrice = droidsTheyWant * pricePerDroid; 
+//     let droidsBuy = credits - totalPrice; 
+//     alert("Вы купили ", droidsTheyWant, " дроидов, на счету осталось ", droidsBuy, " кредитов."); 
+// } else if (droidsTheyWant > 0 > droidsWeHave) { 
+//     alert("Извините, у нас осталось всего ", droidsWeHave, " дроидов, хотите их заказать? Введите друго колличество!"); 
+// } else if (totalPrice > credits) { 
+//     alert("Недостаточно средств на счету!"); }
+
+<!-- M1-HW1-Task5 -->
