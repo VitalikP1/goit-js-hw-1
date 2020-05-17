38995f1d26633832dbda283a0c29a2a4e6966e6f
Vitalik <v.podorozhnyuk@hotmail.com>
2020-05-17 17:55:31 +0300

    innit

--- Task-6.js
+++ Task-6.js
@@ -2,22 +2,22 @@ let input;
 let total = 0;
 
 while (true) {
-    input = prompt('Введите число!');
+  input = prompt("Введите число!");
 
-    if (input === null) {
-        console.log('Отменено пользователем!')
-        break;
-    }
+  if (input === null) {
+    console.log("Отменено пользователем!");
+    break;
+  }
 
-    input = Number(input);
+  input = Number(input);
 
-    const notNumber = Number.isNaN(input);
+  const notNumber = Number.isNaN(input);
 
-    if (notNumber) {
-        console.log('Было введено не число, попробуйте еще раз');
-        continue;
-    }
+  if (notNumber) {
+    console.log("Было введено не число, попробуйте еще раз");
+    continue;
+  }
 
-    total += input;
+  total += input;
 }
-console.log('Общая сумма чисел равна ', total);
+console.log("Общая сумма чисел равна ", total);
